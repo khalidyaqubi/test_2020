220eededdb1e6d16607f17ae673a340f1ae7a66b
kahlidYcoubi <37149129+kahlidYcoubi@users.noreply.github.com>
2020-06-16 10:29:08 +0300

    laravel test

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\City;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+       // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $cities = City::withcount('hospitals')->get();
+        //dd($cities);
+        return view('home',compact('cities'));
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">city</th>
+                                <th scope="col">hospital count</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($cities as $item)
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>{{$item->name}}</td>
+                                    <td>{{$item->hospitals_count}}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
